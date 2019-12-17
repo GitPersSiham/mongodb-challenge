a4d992b8bd20901f2a16afcbf0615c5fc552f7b3
GitPersSiham <lahouali.siham@outlook.fr>
2019-12-17 10:00:04 +0100

    added dontenv and .env

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
+const dotenv =require('dotenv')
  
+dotenv.config();
 // Inclusion de Mongoose
 var mongoose = require('mongoose');
  require('./src/models/profile')
 // On se connecte à la base de données
 
-mongoose.connect('mongodb://localhost/blog', function(err) {
+mongoose.connect(process.env.MONGODB_URI,{ useNewUrlParser: true } ,function(err) {
   if (err) { throw err; }
 });
  
